f43a5b4514de2287f964fd521cef60a42d2d4d02
Paola Contreras <paolacontreras231@gmail.com>
2022-10-07 09:56:53 -0600

    done

--- Raytracer.py
+++ Raytracer.py
@@ -14,8 +14,6 @@ stone = Material(diffuse = (0.4, 0.4, 0.4), spec = 8)
 grass = Material(diffuse = (0.3, 1.0, 0.3), spec = 64)
 
 
-marble = Material(spec = 64, texture = Texture("whiteMarble.bmp"))
-
 mirror = Material(diffuse = (0.9, 0.9, 0.9), spec = 64, matType = REFLECTIVE)
 glass = Material(diffuse = (0.9, 0.9, 0.9), spec = 64, ior = 1.5, matType = TRANSPARENT)
 
@@ -25,11 +23,11 @@ rtx.envMap = Texture("parkingLot.bmp")
 
 rtx.lights.append( AmbientLight(intensity = 0.1 ))
 rtx.lights.append( DirectionalLight(direction = (0,0,-1), intensity = 0.5 ))
-rtx.lights.append( PointLight(point = (-1,-1,0) ))
+rtx.lights.append( PointLight(point = (1,-1,0) ))
 
 
 rtx.scene.append( Torus2D(position = (1,-1,-7), radius = 1, radius2 =0.3, normal = (0,0,1), material = glass ))
-rtx.scene.append( Torus2D(position = (1,2,-7), radius = 1.5, radius2 =0.8, normal = (0,0,1), material = marble ))
+rtx.scene.append( Torus2D(position = (1,2,-7), radius = 1.5, radius2 =0.6, normal = (0,0,1), material = mirror ))
 rtx.scene.append( Torus2D(position = (-2,1.5,-7), radius = 2, radius2 =0.9, normal = (0,0,1), material = brick ))
 rtx.glRender()
 
